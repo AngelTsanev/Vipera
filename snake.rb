9b944ce465c218c6d0f326d04716a369dc3d3ae8
Angel Tsanev <archa40@gmail.com>
2014-08-15 00:31:27 +0300

    adds move[up, down, left and right] methods

--- snake.rb
+++ snake.rb
@@ -18,20 +18,54 @@ class Snake:
     return @food_eaten
   end
 
+  def eat_food
+    @food_eaten = true
+  end
+
+  def done_eating_food
+    @food_eaten = false
+  end
+
   def move_up
     head_x, head_y = get_head
     @body.insert(0, [head_x, head_y-1])
-    @body.delete_at(-1) unless food_eaten?
+    if food_eaten?
+      done_eating_food
+    else
+      @body.delete_at(-1)
+    end
   end
 
   def move_down
+    head_x, head_y = get_head
+    @body.insert(0, [head_x, head_y-1])
+    if food_eaten?
+      done_eating_food
+    else
+      @body.delete_at(-1)
+    end
   end
 
   def move_left
+    head_x, head_y = get_head
+    @body.insert(0, [head_x-1, head_y])
+    if food_eaten?
+      done_eating_food
+    else
+      @body.delete_at(-1)
+    end
   end
 
   def move_right
+    head_x, head_y = get_head
+    @body.insert(0, [head_x+1, head_y])
+    if food_eaten?
+      done_eating_food
+    else
+      @body.delete_at(-1)
+    end
   end 
+
 end
 
 
