0c8235b58d7676f052fb7b170ffedebcb7531f03
Angel Tsanev <archa40@gmail.com>
2014-08-28 14:06:40 +0300

    removes some comments

--- map.rb
+++ map.rb
@@ -1,19 +1,18 @@
 class Map
   attr_reader :width, :height, :pixels
   
-  def initialize(mapname)
+  def initialize(map_name)
     @pixels = {}
-    read_map(mapname)
+    read_map(map_name)
   end
 
-  def read_map(mapname)
-    file = IO.readlines("./Maps/" + mapname + ".vmp")
+  def read_map(map_name)
+    file = IO.readlines("./Maps/" + map_name + ".vmp")
     @width = file[0].tr("'\n", "").length
     @height = file.size
     0.upto(file.size.pred) do |y|
-    #puts "#{file[x].tr("'\n", "").length} -> #{file[x]}"
-      0.upto(file[y].size-2) do |x|
-        set_pixel(x, y) if file[y][x].eql?(1)
+      0.upto(file[y].size) do |x|
+        set_pixel(x, y) if file[y][x].eql?('1')
       end
     end
   end
