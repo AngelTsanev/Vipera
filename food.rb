a5c25b1fc806006840fd07b482b96b3fbb0cab99
Angel Tsanev <archa40@gmail.com>
2014-08-28 13:59:58 +0300

    Food class no longer has attr map

--- food.rb
+++ food.rb
@@ -1,18 +1,16 @@
 class Food
-  attr_reader :x, :y, :map 
+  attr_reader :x, :y
 
-  def initialize(map)
-    @map = map
-    set_food
+  def initialize()
+    x = 0
+    y = 0
   end
 
-  def set_food
-    x = rand(map.width)
-    y = rand(map.height)
-    while @map.pixel_at?(x, y) and @snake.pixel_at?(x, y)
-      x = rand(map.width)
-      y = rand(map.height)
-    end
+  def set_food(x, y)
     @x, @y = x, y
+  end
+
+  def food_at?(x, y)
+    x.eql?(@x) and y.eql?(@y)
   end
 end
