6939e04f32948af335e85f626519312d565edab2
Angel Tsanev <archa40@gmail.com>
2014-08-30 09:07:03 +0300

    now if multiplayer save_new_highscore method makes two new entries

--- highscore.rb
+++ highscore.rb
@@ -7,35 +7,48 @@ class Player
   end
 
   def inspect
-    @name + " -> " + @score.to_s
+    "   " + @name + " -> " + @score.to_s
   end
 end
 
 
-def read_new_score(score)
-  puts "Enter your Nickname :"
-  name = gets.chomp
+def read_new_score(score, player)
+  puts "Enter your nickname, player " + player.to_s + " : "
+  name = STDIN.gets.chomp
   Player.new(name, score)
 end
 
 def read_all_highscores
   highscores = []
-  File.open("./Highscores/highscores.txt", 'r') do |file|  
-  while name = file.gets
-    name = name[0...-1] 
-    score  = file.gets
-    highscores.insert(-1, Player.new(name, score.to_i))
+  File.open("./Highscores/highscores.txt", 'r') do |file|
+    while name = file.gets
+      name = name[0...-1]
+      score  = file.gets
+      highscores.insert(-1, Player.new(name, score.to_i))
+    end
   end
-  #p highscores.sort_by!(&:score).reverse!
   highscores
-end  
-
 end
 
-def save_ten_top_highscores()
-  new_score = read_new_score(192)
+def print_highscores
+  system "clear"
+  close_screen
   highscores = read_all_highscores
+  puts "Highscores :"
+  0.upto(highscores.size.pred) do |i|
+    p highscores[i]
+  end
+end
+
+def save_new_highscores(score_one, score_two)
+  highscores = read_all_highscores
+  new_score = read_new_score(score_one, 1)
   highscores.insert(-1, new_score)
+  if score_two != -1
+    new_score = read_new_score(score_two, 2)
+    highscores.insert(-1, new_score)
+  end
+
   highscores.sort_by!(&:score).reverse!
   open('./Highscores/highscores.txt', 'w') do |file|
     0.upto([10, highscores.size].min.pred) do |entry|
@@ -44,5 +57,3 @@ def save_ten_top_highscores()
     end
   end
 end
-
-save_ten_top_highscores
